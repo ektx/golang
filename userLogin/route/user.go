6ec46d5bf0f81522a486e7b8396056619c362f92
wenlong <530675800@qq.com>
2020-04-20 22:43:42 +0800

    添加错误处理

--- userLogin/route/user.go
+++ userLogin/route/user.go
@@ -6,19 +6,23 @@ import (
   "github.com/go-playground/validator/v10"
   "log"
   "net/http"
-  "userLogin/common"
   "userLogin/model"
 )
 
 func addUser(c *gin.Context) {
-  db := common.GetDB()
   
   var u model.UserInfo
+  ageErrInfo := map[string]string {
+    "required": "年龄不能为空",
+    "gte": "年龄不能小于 0",
+    "lte": "年龄不能大于 130",
+  }
   log.Print(1, &u)
   
   // 绑定值
   if err := c.ShouldBind(&u); err != nil {
     fmt.Printf("%#v", err)
+    var msg = ""
     
     for _, err := range err.(validator.ValidationErrors) {
       fmt.Println(err.Namespace())
@@ -31,16 +35,24 @@ func addUser(c *gin.Context) {
       fmt.Println(err.Type())
       fmt.Println(err.Value())
       fmt.Println(err.Param())
-      fmt.Println()
+  
+      switch err.StructField() {
+        case "Name": msg = "用户名不能为空"
+        case "Gender": msg = "性别不能为空"
+        case "Age": msg = ageErrInfo[err.Tag()]
+      }
+      fmt.Println(msg)
+  
+      c.JSON(http.StatusBadRequest, gin.H{
+        "msg": msg,
+      })
+      break
     }
-  
-  
-    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
     return
   }
   
   // 保存
-  if err := db.Debug().Create(&u).Error; err != nil {
+  if err := model.SaveUserInfo(&u); err != nil {
     c.JSON(http.StatusOK, gin.H{
       "msg": "添加失败",
     })
